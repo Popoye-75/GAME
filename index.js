e536467abc52fd6ab33681250f3cdb1267e76efc
Popoye-75 <kasifmohd818@gmail.com>
2025-08-31 23:12:09 +0530

    updated js , html file

--- index.js
+++ index.js
@@ -4,9 +4,15 @@ let signUpButton = document.getElementById("signInterface"); // Select the sign 
 const showLogin = () => { // Function for shows the login interface and hide signup 
     loginButton.style.display = "flex"; // Enable login interface
     signUpButton.style.display = "none"; // Disable signup interface
-
 }
 const showSignUp = () => { // Function for shows the signup interface and hide login
     signUpButton.style.display = "flex"; // Enable signup interface
     loginButton.style.display = "none";  // Disable login interface
+}
+const trialGame = () => { // Function for redirect on another game web
+    location = ("http://127.0.0.1:5500/numberhunt.html"); // When user press the playnow button this will redirect to game web
+}
+const generateRandom = () => {
+    let random = Math.floor(Math.random() * 100);
+    return random;
 }
